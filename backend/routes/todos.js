06909d32557d07684d2c81ba152429e2dedeaeb1
anandakbari <anandakbari9@gmail.com>
2025-11-19 16:12:41 +0530

    Removed backend folder from this branch

--- backend/routes/todos.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const {getAllTodos, createTodo, getTodo, updateTodo, deleteTodo}=require('../controller/todos')
-router.route('/').get(getAllTodos).post(createTodo)
-router.route('/:id').get(getTodo).put(updateTodo).delete(deleteTodo)
-
-module.exports=router
